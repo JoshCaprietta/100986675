d2bcbbd3fa1329f7c03ff413039df14e1608b82a
JoshCaprietta <joshua.caprietta@dcmail.ca>
2025-03-31 20:55:47 -0400

    Part 2 Final

--- Assignment 4/JavaScript Part 2/main.js
+++ Assignment 4/JavaScript Part 2/main.js
@@ -24,4 +24,14 @@ for (let i = 1; i <= 5; i++) {
 }
 
 /* Wiring up the Darken/Lighten button */
-
+btn.addEventListener('click', () => {
+    if (overlay.style.backgroundColor === 'rgba(0, 0, 0, 0.5)') {
+        overlay.style.backgroundColor = 'rgba(0, 0, 0, 0)'; // Lighten
+        btn.setAttribute("class", "light-mode"); // Change button class
+        btn.textContent = "Darken";
+    } else {
+        overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)'; // Darken
+        btn.setAttribute("class", "dark-mode"); // Change button class
+        btn.textContent = "Lighten";
+    }
+});
